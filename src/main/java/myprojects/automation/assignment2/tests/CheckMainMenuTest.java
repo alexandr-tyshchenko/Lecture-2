b029c3442f22ca4305f3fcce529e4c98a8617099
Alex.Tyshchenko <alex.tyshchenko89@gmail.com>
2017-04-06 09:08:34 +0300

    chfnges

--- src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
@@ -44,105 +44,105 @@ public class CheckMainMenuTest extends BaseScript {
 
         WebElement dashboard = driver.findElement(By.id("tab-AdminDashboard"));
         dashboard.click();
+        String dashboardPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String dashboardPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(dashboardPageTitle.contains(successDashboardTitle));
         sleep(5000);
 
         WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
         orders.click();
+        String ordersPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String ordersPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(ordersPageTitle.contains(successOrdersTitle));
         sleep(5000);
 
         WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
         catalog.click();
+        String catalogPageTitle = driver.findElement(By.className("title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String catalogPageTitle = driver.findElement(By.className("title")).getText();
         System.out.println(catalogPageTitle.contains(successCatalogTitle));
         sleep(5000);
 
         WebElement clients = driver.findElement(By.partialLinkText("Клиенты"));
         clients.click();
+        String clientsPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String clientsPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(clientsPageTitle.contains(successClientsTitle));
         sleep(5000);
 
         WebElement service = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
         service.click();
+        String servicePageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String servicePageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(servicePageTitle.contains(successServiceTitle));
         sleep(5000);
 
         WebElement stats = driver.findElement(By.id("subtab-AdminStats"));
         stats.click();
+        String statsPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String statsPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(statsPageTitle.contains(successStatsTitle));
         sleep(5000);
 
         WebElement modules = driver.findElement(By.id("subtab-AdminParentModulesSf"));
         modules.click();
+        String modulesPageTitle = driver.findElement(By.className("title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String modulesPageTitle = driver.findElement(By.className("title")).getText();
         System.out.println(modulesPageTitle.contains(successModulesTitle));
         sleep(5000);
 
         WebElement design = driver.findElement(By.linkText("Design"));
         design.click();
+        String designPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String designPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(designPageTitle.contains(successDesignTitle));
         sleep(5000);
 
         WebElement shipping = driver.findElement(By.id("subtab-AdminParentShipping"));
         shipping.click();
+        String shippingPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String shippingPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(shippingPageTitle.contains(successShippingTitle));
         sleep(5000);
 
         WebElement payment = driver.findElement(By.id("subtab-AdminParentPayment"));
         payment.click();
+        String paymentPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String paymentPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(paymentPageTitle.contains(successPaymentTitle));
         sleep(5000);
 
         WebElement international  = driver.findElement(By.id("subtab-AdminInternational"));
         international.click();
+        String internationalPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String internationalPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(internationalPageTitle.contains(successInternationalTitle));
         sleep(5000);
 
         WebElement parameters  = driver.findElement(By.id("subtab-ShopParameters"));
         parameters.click();
+        String parametersPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String parametersPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(parametersPageTitle.contains(successParametersTitle));
         sleep(5000);
 
         WebElement config  = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
         config.click();
+        String configPageTitle = driver.findElement(By.className("page-title")).getText();
         driver.navigate().refresh();
         sleep(5000);
-        String configPageTitle = driver.findElement(By.className("page-title")).getText();
         System.out.println(configPageTitle.contains(successConfigTitle));
         sleep(5000);
 
