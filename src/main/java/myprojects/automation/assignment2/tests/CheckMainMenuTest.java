ad6be2e9df0fb0d7bd430a27df666b71b23a6062
Alex.Tyshchenko <alex.tyshchenko89@gmail.com>
2017-04-09 20:00:02 +0300

    changes from 09/04/17

--- src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/CheckMainMenuTest.java
@@ -1,14 +1,14 @@
 package myprojects.automation.assignment2.tests;
 
-import myprojects.automation.assignment2.utils.BaseScript;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import static java.lang.Thread.sleep;
+        import myprojects.automation.assignment2.utils.BaseScript;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import java.util.ArrayList;
+        import java.util.List;
 
 /**
- * Created by Alexandr on 03.04.2017.
+ * Created by Alexandr on 08.04.2017.
  */
 public class CheckMainMenuTest extends BaseScript {
 
@@ -27,129 +27,26 @@ public class CheckMainMenuTest extends BaseScript {
 
         sleep(1000);
 
-        String successDashboardTitle = "Пульт";
-        String successOrdersTitle = "Заказы";
-        String successCatalogTitle = "товары";
-        String successClientsTitle = "Управление клиентами";
-        String successServiceTitle = "Customer Service";
-        String successStatsTitle = "Статистика";
-        String successModulesTitle = "Выбор модуля";
-        String successDesignTitle = "Шаблоны > Шаблон";
-        String successShippingTitle = "Перевозчики";
-        String successPaymentTitle = "Payment Methods";
-        String successInternationalTitle = "Локализация";
-        String successParametersTitle = "General";
-        String successConfigTitle = "Information";
+        List<WebElement> menu = driver.findElements(By.className("maintab"));
+        ArrayList<String> textLinkList = new ArrayList<String>();
 
+        for(WebElement e : menu){
+            textLinkList.add(e.getText());
+        }
 
-        WebElement dashboard = driver.findElement(By.id("tab-AdminDashboard"));
-        dashboard.click();
-        String dashboardPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(dashboardPageTitle.contains(successDashboardTitle));
-        sleep(5000);
-
-        WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
-        orders.click();
-        String ordersPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(ordersPageTitle.contains(successOrdersTitle));
-        sleep(5000);
-
-        WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
-        catalog.click();
-        String catalogPageTitle = driver.findElement(By.className("title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(catalogPageTitle.contains(successCatalogTitle));
-        sleep(5000);
-
-        WebElement clients = driver.findElement(By.partialLinkText("Клиенты"));
-        clients.click();
-        String clientsPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(clientsPageTitle.contains(successClientsTitle));
-        sleep(5000);
-
-        WebElement service = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
-        service.click();
-        String servicePageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(servicePageTitle.contains(successServiceTitle));
-        sleep(5000);
-
-        WebElement stats = driver.findElement(By.id("subtab-AdminStats"));
-        stats.click();
-        String statsPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(statsPageTitle.contains(successStatsTitle));
-        sleep(5000);
-
-        WebElement modules = driver.findElement(By.id("subtab-AdminParentModulesSf"));
-        modules.click();
-        String modulesPageTitle = driver.findElement(By.className("title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(modulesPageTitle.contains(successModulesTitle));
-        sleep(5000);
-
-        WebElement design = driver.findElement(By.linkText("Design"));
-        design.click();
-        String designPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(designPageTitle.contains(successDesignTitle));
-        sleep(5000);
-
-        WebElement shipping = driver.findElement(By.id("subtab-AdminParentShipping"));
-        shipping.click();
-        String shippingPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(shippingPageTitle.contains(successShippingTitle));
-        sleep(5000);
-
-        WebElement payment = driver.findElement(By.id("subtab-AdminParentPayment"));
-        payment.click();
-        String paymentPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(paymentPageTitle.contains(successPaymentTitle));
-        sleep(5000);
-
-        WebElement international  = driver.findElement(By.id("subtab-AdminInternational"));
-        international.click();
-        String internationalPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(internationalPageTitle.contains(successInternationalTitle));
-        sleep(5000);
-
-        WebElement parameters  = driver.findElement(By.id("subtab-ShopParameters"));
-        parameters.click();
-        String parametersPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(parametersPageTitle.contains(successParametersTitle));
-        sleep(5000);
-
-        WebElement config  = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
-        config.click();
-        String configPageTitle = driver.findElement(By.className("page-title")).getText();
-        driver.navigate().refresh();
-        sleep(5000);
-        System.out.println(configPageTitle.contains(successConfigTitle));
-        sleep(5000);
-
+        for(String s : textLinkList){
+            driver.findElement(By.linkText(s)).click();
+            sleep(5000);
+            String title = driver.getTitle();
+            System.out.println(title);
+            driver.navigate().refresh();
+            sleep(5000);
+            if(!title.equals(driver.getTitle())) System.out.println("Page title was changed");
+        }
         driver.quit();
     }
 
-    public static void sleep(long ms) {
+    public static void sleep (long ms) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
